66bcaec1e7872c044aaea86101b9b8162269ad3f
LSDdesign <v.leino@yandex.ru>
2014-03-05 13:28:08 +0400

    +
    +

---
+++ tryruby/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tryruby::Application.config.secret_key_base = '5eecb843cff091990e316fe88b6a77379efd7e6d64f6b5d39d441a6c843cb1f15c7583e4e297a1e85a27e3323a44ca48993235aa9e4b751a5e5bf6610c244659'
---
+++ tryruby/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tryruby::Application.config.session_store :cookie_store, key: '_tryruby_session'
